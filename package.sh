2809dc89939683abff9e10f4e40080534829aa57
Gonzalo Aguilar Delgado <gaguilar@level2crm.com>
2020-08-05 23:17:51 +0200

    Add package tool

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATE=$(date +%Y%m%d%H%M%S)
+FILENAME="../server-cde-branding-$DATE.tar"
+FILENAME_APPS="../server-cde-branding-apps-$DATE.tar"
+
+echo "Packaging $FILENAME"
+tar -cf $FILENAME --exclude=".git*" --exclude="config" --exclude="tests" --exclude="apps/*" --exclude="3rdparty/*" --exclude="build/*" *
+pbzip2 $FILENAME
+
+tar -cf $FILENAME_APPS --exclude="apps/files_ifcviewer" --exclude="apps/files_ifcviewer_annotation" --exclude="apps/workflow_ifc_converter" --exclude="apps/workflow_pdf_converter" apps/*
+pbzip2 $FILENAME_APPS
