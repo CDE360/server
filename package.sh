5a31478d2224d123c7de7b49e9732ef11c84f5f3
Gonzalo Aguilar Delgado <gaguilar@level2crm.com>
2020-09-15 14:52:13 +0200

    Exclude data

--- package.sh
+++ package.sh
@@ -5,8 +5,9 @@ FILENAME="../server-cde-branding-$DATE.tar"
 FILENAME_APPS="../server-cde-branding-apps-$DATE.tar"
 
 echo "Packaging $FILENAME"
-tar -cf $FILENAME --exclude=".git*" --exclude="config" --exclude="tests" --exclude="apps/*" --exclude="3rdparty/*" --exclude="build/*" *
-pbzip2 $FILENAME
+tar -cf $FILENAME --exclude=".git*" --exclude="config" --exclude="tests" --exclude="apps/*" --exclude="3rdparty/*" --exclude="build/*" --exclude="data" *
+pbzip2 $FILENAME &
 
+echo "Packaging $FILENAME_APPS"
 tar -cf $FILENAME_APPS --exclude="apps/files_ifcviewer" --exclude="apps/files_ifcviewer_annotation" --exclude="apps/workflow_ifc_converter" --exclude="apps/workflow_pdf_converter" apps/*
 pbzip2 $FILENAME_APPS
